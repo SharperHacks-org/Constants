8391e3c2121ed34c1e6ea57e4a6a15ca0410debd
Joseph W Donahue <joseph@sharperhacks.com>
2023-11-18 21:35:45 -0800

    BC: Update dependencies and mormalize project file.

--- Constants/TimeConstants.cs
+++ Constants/TimeConstants.cs
@@ -16,7 +16,7 @@
 // applied to distributions of derivative works, without the express written
 // permission of a registered officer of Sharper Hacks LLC (US-WA).
 
-namespace SharperHacks.CoreLibes.Constants;
+namespace SharperHacks.CoreLibs.Constants;
 
 /// <summary>
 /// A collection of useful time constants.
