24af9606cc7d443c2eda45026f26f75d4b96b176
Joseph W Donahue <joseph@sharperhacksllc.com>
2025-07-17 20:30:36 -0700

    Feature: Added AsciiControlCodes.cs.

---
+++ Constants/AsciiControlCodes.cs
@@ -0,0 +1,64 @@
+// Copyright and trademark notices at the end of this file.
+
+
+namespace SharperHacks.CoreLibs.Constants;
+
+/// <summary>
+/// ASCII control codes. First 31 symbols of the ASCII table.
+/// <remarks>
+/// Ripped from https://www.asciitable.com/
+/// </remarks>
+/// </summary>
+public static class AsciiControlCodes
+{
+    public const char NUL = (char)0;
+    public const char SOH = (char)1;
+    public const char STX = (char)2;
+    public const char ETX = (char)3;
+    public const char EOT = (char)4;
+    public const char ENQ = (char)5;
+    public const char ACK = (char)6;
+    public const char BEL = (char)7;
+    public const char BS = (char)8;
+    public const char TAB = (char)9;
+    public const char LF = (char)10;
+    public const char VT = (char)11;
+    public const char FF = (char)12;
+    public const char CR = (char)13;
+    public const char SO = (char)14;
+    public const char SI = (char)15;
+    public const char DLE = (char)16;
+    public const char DC1 = (char)17;
+    public const char DC2 = (char)18;
+    public const char DC3 = (char)19;
+    public const char DC4 = (char)20;
+    public const char NAK = (char)21;
+    public const char SYN = (char)22;
+    public const char ETB = (char)23;
+    public const char CAN = (char)24;
+    public const char EM = (char)25;
+    public const char SUB = (char)26;
+    public const char ESC = (char)27;
+    public const char FS = (char)28;
+    public const char GS = (char)29;
+    public const char RS = (char)30;
+    public const char US = (char)31;
+}
+
+// Copyright Joseph W Donahue and Sharper Hacks LLC (US-WA)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SharperHacks is a trademark of Sharper Hacks LLC (US-Wa), and may not be
+// applied to distributions of derivative works, without the express written
+// permission of a registered officer of Sharper Hacks LLC (US-WA).
